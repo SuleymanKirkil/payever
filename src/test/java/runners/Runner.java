352bdf70f83cf086db00a499b9c39d5607cf3dc9
Süleyman Kırkıl <skirkil43@gmail.com>
2023-03-21 03:55:40 +0300

    Closing the browser

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
                 //"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
         features = "./src/test/resources/features/",
-        glue = {"stepDefinitions"},
+        glue = {"stepDefinitions","hooks"},
         tags = "@US_01_registiration",
         dryRun =false
 )
